3d5277098a73a40f91ba9a8c052be5fc424fe1b8
gabriel0263 <167627055+gabriel0263@users.noreply.github.com>
2025-08-27 19:36:42 -0300

    Update desafio_9.py
    O que você fez imprime só uma linha da tabuada (n x 1).
    Para mostrar toda a tabuada de 1 a 10, você precisa usar um laço for.

--- mundo_1/Aula_7_Calculos_e_Operacoes/desafio_9.py
+++ mundo_1/Aula_7_Calculos_e_Operacoes/desafio_9.py
@@ -7,3 +7,9 @@ print("Tabuada do {}:".format(n))
 print("{} x {} = {}".format(n, 1, (n * 1)))
 
 # Sem loop o código é repetitivo
+# Para tabuada completa do 1 ao 10
+n = int(input('Digite um numero: '))
+print(f'Tabuada do {n}:')
+
+for i in range(1, 11):
+     print(f'{n} x {i} = {n * i}')
